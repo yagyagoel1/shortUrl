05ed9983e79c024749211fc02b0db1cab9810cd6
yagyagoel1 <yagyagoel87@gmail.com>
2024-10-06 06:21:09 +0530

    edited the gitignore

--- backend/dist/routes/url.route.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const url_controller_1 = require("../controller/url.controller");
-const router = (0, express_1.Router)();
-router.route("/url/create").post(url_controller_1.createUrl);
-exports.default = router;
