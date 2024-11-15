e2e3d605bc332f14ef15eedfb4b919a10e5d5dc5
yagyagoel1 <yagyagoel87@gmail.com>
2024-11-16 04:52:23 +0530

    commetned

--- backend/src/utils/apiResponse.ts
+++ backend/src/utils/apiResponse.ts
@@ -11,5 +11,5 @@ class ApiResponse {
       this.success = status < 400;
     }
   }
-  
+  // docker run cloudflare/cloudflared:latest tunnel --no-autoupdate run --token eyJhIjoiYjA1M2JlNDk2NGEzNzU0MjU1ZjFiM2NmMTc5NTAyZWQiLCJ0IjoiMzI2M2U1ZDAtNGYzOC00MGY2LThlMzktNTcxNTA0MWI5ZmQ5IiwicyI6Ik4yTmtPREl6T1RRdFl6UmlNUzAwT0RKbExUaG1abVl0TmpBME0yVmtZelkwWVdNMiJ9
   export { ApiResponse };
